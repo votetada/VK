af0c222e456c61d81dfef3a66270837e29e6a83b
votetada <ytyulkin@bk.ru>
2021-07-05 16:15:42 +0300

    Добавить класс

---
+++ VK/user.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VK
+{
+    class user
+    {
+        string _Name;
+        string _Information;
+
+        user(string Name, string Information)
+        {
+            _Name = Name;
+            _Information = Information;
+        }
+    }
+}
