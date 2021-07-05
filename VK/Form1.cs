5cb1002b7cd16530b06e85ae50f5657f471f89e4
votetada <ytyulkin@bk.ru>
2021-07-05 15:38:52 +0300

    Пакет VK

--- VK/Form1.cs
+++ VK/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace VK
 {
@@ -15,6 +16,43 @@ namespace VK
         public Form1()
         {
             InitializeComponent();
+        }
+        public static string getAuthForGroup()
+        {
+            string fileName = @"auth_vk.txt";
+            string token = "";
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    token = sr.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return token;
+        }
+
+        // получить токен из файла
+        public static string getAuthForUser()
+        {
+            string fileName = @"auth_vk.txt";
+            string token = "";
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    sr.ReadLine();
+                    token = sr.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return token;
         }
     }
 }
