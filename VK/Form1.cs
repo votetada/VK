0e56d207b5013e961cc812f9f0a3c4b766e44413
votetada <ytyulkin@bk.ru>
2021-07-05 16:17:13 +0300

    Revert "Добавить класс"
    This reverts commit af0c222e456c61d81dfef3a66270837e29e6a83b.

--- VK/Form1.cs
+++ VK/Form1.cs
@@ -8,9 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
-using VkNet;
-using VkNet.Model;
-using VkNet.Model.RequestParams;
 
 namespace VK
 {
@@ -20,7 +17,7 @@ namespace VK
         {
             InitializeComponent();
         }
-        public static string getAuthForUser()
+        public static string getAuthForGroup()
         {
             string fileName = @"auth_vk.txt";
             string token = "";
@@ -39,7 +36,7 @@ namespace VK
         }
 
         // получить токен из файла
-        public static string getAuthForGroup()
+        public static string getAuthForUser()
         {
             string fileName = @"auth_vk.txt";
             string token = "";
@@ -56,39 +53,6 @@ namespace VK
                 Console.WriteLine(e.Message);
             }
             return token;
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            var api_user = new VkApi();
-            // обработать исключения!
-            api_user.Authorize(new ApiAuthParams
-            {
-                AccessToken = getAuthForUser()
-            });
-
-
-            var getFriends = api_user.Friends.Get(new VkNet.Model.RequestParams.FriendsGetParams
-            {
-                Fields = VkNet.Enums.Filters.ProfileFields.All
-            });
-            string s = "";
-            foreach (User user in getFriends)
-            {
-                s += user.FirstName + user.LastName + "\n";
-                s += user.Sex + "\n";
-                s += user.Relation + "\n";
-                s += user.HasMobile;
-                s += user.BirthDate;
-                listBox1.Items.Add(s);
-                s = "";
-            }
-            
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
